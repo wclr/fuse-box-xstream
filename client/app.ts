87e290d04be759e83a2f98ec3fead76ff2e4ec00
whitecolor <alex.oshchepkov@gmail.com>
2017-05-17 23:09:44 +0500

    lines should be commented

--- client/app.ts
+++ client/app.ts
@@ -1,7 +1,7 @@
 console.log('app here')
 
-import debounce from 'xstream/extra/debounce'
-debounce(100)
+// import debounce from 'xstream/extra/debounce'
+// debounce(100)
 
-import throttle from 'xstream/extra/throttle'
-throttle(100)
+// import throttle from 'xstream/extra/throttle'
+// throttle(100)
